167b5c27b399a3635270cc377a8600d71f7935ea
Zhongshang Ersi <1759961798ngl@gmail.com>
2025-01-18 01:03:36 +0800

    登录页面

--- src/pages/login/login.vue
+++ src/pages/login/login.vue
@@ -1,7 +1,39 @@
 <template>
-  <view> This is login page. </view>
+  <view class="h-screen w-screen flex justify-center items-center p-12">
+    <view class="w-full">
+      <wd-button :block="true" icon="phone" size="large" @tap="login">
+        <text class="font-light">手机号一键登录</text>
+      </wd-button>
+      <wd-divider> 其他登录方式</wd-divider>
+      <view class="w-full flex flex-col justify-center items-center">
+        <button
+          class="border border-solid border-black w-10 h-10 rounded-full flex items-center justify-center"
+          @tap="loginSuccess"
+        >
+          <wd-icon name="phone" size="22px"></wd-icon>
+        </button>
+        <text class="text-sm mt-1">模拟登录</text>
+      </view>
+    </view>
+  </view>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const login = () => {
+  uni.login({
+    provider: "weixin",
+    success: (res) => {
+      console.log(res.code);
+      loginSuccess();
+    },
+  });
+};
+
+async function loginSuccess() {
+  await uni.showToast({ icon: "success", title: "登录成功" });
+  //TODO save userInfo
+  setTimeout(() => uni.switchTab({ url: "/pages/index/index" }), 800);
+}
+</script>
 
 <style scoped lang="scss"></style>
