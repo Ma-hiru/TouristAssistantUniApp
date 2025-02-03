6c1b7743153b7f414bdc13d9e41972d8936073df
Zhongshang Ersi <1759961798ngl@gmail.com>
2025-02-03 20:18:27 +0800

    压缩体积

--- src/wxcomponents/towxml/index.d.ts
+++
@@ -1,8 +0,0 @@
-declare export const useTowxml = (str: string, type: "markdown" | "html", option?: {
-  base?: string;
-  theme?: string | "light" | "dark";
-  events?: {
-    [k: string]: (e:any) => void;
-  };
-  [k: string]: any;
-}) => any;
