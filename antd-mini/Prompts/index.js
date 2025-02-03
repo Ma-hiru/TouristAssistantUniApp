dcd1a0e8e8c9db0777a13d5ca89800ae556ae654
Zhongshang Ersi <1759961798ngl@gmail.com>
2025-02-03 20:25:41 +0800

    压缩体积

--- antd-mini/Prompts/index.js
+++
@@ -1,9 +0,0 @@
-import { Component, triggerEventValues } from "../_util/simply";
-import { PromptsProps } from "./props";
-
-Component(PromptsProps, {
-  onItemTap: function (e) {
-    const { item, index } = e.currentTarget.dataset;
-    triggerEventValues(this, "itemtap", [item, index], e);
-  },
-});
