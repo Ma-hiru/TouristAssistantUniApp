6a7133774e81929c022971ef60e2f9130713dad6
Ma-hiru <tenshimahiru@qq.com>
2025-06-11 23:50:56 +0800

    优化细节

--- src/components/Header/Header.vue
+++ src/components/Header/Header.vue
@@ -1,13 +1,26 @@
 <template>
   <view class="bg-white w-screen top-0 left-0 z-[60] fixed">
     <view
-      class="text-center text-base font-bold flex justify-center items-center"
+      class="text-center text-base font-bold flex justify-center items-center relative"
       :style="{
         height: $slots.default ? `100rpx` : `120rpx`,
         marginTop: `${top}rpx`,
         paddingTop: `25rpx`,
       }"
     >
+      <view
+        @tap="goBack"
+        class="back"
+        :style="{
+          height: $slots.default ? `100rpx` : `120rpx`,
+          paddingTop: `25rpx`,
+          paddingLeft: `25rpx`,
+        }"
+      >
+        <button v-show="back" class="clear-btn">
+          <uni-icons type="arrow-left" size="25" />
+        </button>
+      </view>
       <text>{{ title }}</text>
     </view>
     <view class="w-screen relative">
@@ -24,17 +37,32 @@
 
 <script setup lang="ts" name="Header">
 const { top } = uni.getMenuButtonBoundingClientRect();
-const { title, slotHeight, fill } = withDefaults(
+const { title, slotHeight, fill, back } = withDefaults(
   defineProps<{
     title: string;
     slotHeight?: number;
     fill?: boolean;
+    back?: boolean;
   }>(),
   {
     slotHeight: 0,
     fill: false,
+    back: false,
   }
 );
+
+const goBack = () => {
+  uni.navigateBack();
+};
 </script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.back {
+  position: absolute;
+  display: flex;
+  justify-content: start;
+  align-items: center;
+  left: 0;
+  top: 0;
+}
+</style>
